c4207b5797f4d6e323f524ffa206d36651376c6b
Евгений Кулижников <evgenijkuliznikov@MacBook-Pro-Evgenij.local>
2024-11-19 20:04:21 +0300

    Написать функцию получения слова с бэкенда

---
+++ src/main/resources/api/config.js
@@ -0,0 +1,14 @@
+export const config = {
+  BASE_URL: 'http://localhost:9090',
+  METHOD: {
+    GET: 'GET',
+    POST: 'POST'
+  },
+  STATUSES: {
+    OK: 200,
+    NO_CONTENT: 204
+  },
+  headers: {
+    'Content-Type': 'application/json'
+  }
+}
---
+++ src/main/resources/api/getWord.js
@@ -0,0 +1,21 @@
+import { config } from './config.js';
+
+export const getWord = async () => {
+  const { BASE_URL, METHOD, STATUSES, headers } = config;
+
+  try {
+    const response = await fetch(`${BASE_URL}/word`, {
+      method: METHOD.GET,
+      headers
+    });
+
+    if (response.status === STATUSES.OK) {
+      return response.json();
+    } else if (response.status === STATUSES.NO_CONTENT) {
+      console.log('Поздравляем, вы отгадали все слова!');
+    }
+
+  } catch (error) {
+    console.log(error, ' Произошла ошибка сервера. Попробуйте позже');
+  }
+}

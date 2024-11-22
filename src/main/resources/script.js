3e7dad92846a89da77c6605e8db4882d7bbdea4a
Sofya Kozlova <sonnik.02@mail.ru>
2024-11-22 16:20:37 +0500

    feature/5: добавлена базовая вёрстка и стили

---
+++ src/main/resources/script.js
@@ -0,0 +1,22 @@
+// Размеры игрового поля
+const boardSizeY = 6;
+const boardSizeX = 5;
+
+const gameBoard = document.querySelector('.game-board');
+
+// Функция отрисовки игрового поля
+const createBoard = () => {
+  for (let i = 0; i < boardSizeY; i++) {
+    const row = document.createElement('ul');
+    row.classList.add('game-board__row');
+
+    for (let j = 0; j < boardSizeX; j++) {
+      const cell = document.createElement('li');
+      cell.classList.add('game-board__cell');
+      row.appendChild(cell);
+    }
+    gameBoard.appendChild(row);
+  }
+};
+
+createBoard();

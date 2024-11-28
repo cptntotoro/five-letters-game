c9ef9f77719d2304e2fcf141056c5b3d0f14f9c1
Sofya Kozlova <sonnik.02@mail.ru>
2024-11-28 19:28:13 +0500

    feature/23: добавлены input'ы в клетки игрового поля, сверстана ошибка ввода другой части речи

--- src/main/resources/scripts/game-board/create-board.js
+++ src/main/resources/scripts/game-board/create-board.js
@@ -12,11 +12,15 @@ export const createBoard = () => {
 
     for (let j = 0; j < boardSizeX; j++) {
       const cell = document.createElement('li');
+      const input = document.createElement('input');
+
       cell.classList.add('game-board__cell');
+      input.classList.add('game-board__input');
+      input.setAttribute('maxlength', '1');
+
       row.appendChild(cell);
+      cell.appendChild(input);
     }
     gameBoard.appendChild(row);
   }
 };
-
-

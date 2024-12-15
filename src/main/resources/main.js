9886d1b3ae4fa481e743ae32eecbee3236c9670f
Евгений Кулижников <evgenijkuliznikov@MacBook-Pro-Evgenij.local>
2024-12-15 19:55:28 +0300

    feature/19-new: добавлена валидация

--- src/main/resources/main.js
+++ src/main/resources/main.js
@@ -2,6 +2,13 @@ import { getWord } from './scripts/api/getWord.js';
 import { createBoard } from './scripts/game-board/create-board.js';
 import { renderLoading } from './scripts/game-board/loader.js';
 import { gameResultShow } from './scripts/game-board/game-result.js';
+import { validateInput } from './scripts/game-board/input-validation.js'
+
+createBoard();
+
+const gameBoardInputs = document.querySelectorAll('.game-board__input');
+
+validateInput(gameBoardInputs);
 
 const logWord = async () => {
   const data = await getWord();
@@ -10,5 +17,4 @@ const logWord = async () => {
   }
 };
 
-createBoard();
 logWord();

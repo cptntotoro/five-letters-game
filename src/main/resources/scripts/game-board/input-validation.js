8d41e62311df7f5a57fc31ce420fef7f8ff91c76
Евгений Кулижников <evgenijkuliznikov@MacBook-Pro-Evgenij.local>
2024-12-15 17:20:23 +0300

    feature/19: refactor

--- src/main/resources/scripts/game-board/input-validation.js
+++ src/main/resources/scripts/game-board/input-validation.js
@@ -1,11 +1,12 @@
+const regex = /[^А-Яа-яЁё]$/;
+
 export const validateInput = (inputs) => {
   inputs.forEach((input) => {
     input.addEventListener('input', (event) => {
       const value = event.target.value;
-      const newValue = value.replace(/[^А-Яа-яЁё]$/, '');
 
-      if (value !== newValue) {
-        event.target.value = newValue;
+      if (regex.test(value)) {
+        event.target.value = '';
       }
     })
   })

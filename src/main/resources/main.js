2b52b13d162af49840bf0dfddf4efcf0c826aa08
Eugene <hidden@mail.ru>
2024-12-18 21:50:03 +0300

    feature/20: расмширен и модифицирован функционал показа экрана результата игры

--- src/main/resources/main.js
+++ src/main/resources/main.js
@@ -3,12 +3,10 @@ import { createBoard } from './scripts/game-board/create-board.js';
 import { renderLoading } from './scripts/game-board/loader.js';
 import { gameResultShow } from './scripts/game-board/game-result.js';
 import { validateInput } from './scripts/game-board/input-validation.js'
-import { blockInputRow } from './scripts/game-board/block-input-row.js';
+import { blockInputRows } from './scripts/game-board/block-input-row.js';
 import { toggleCheckButtonState } from './scripts/game-board/toggle-check-button-state.js';
 import { getWordFromInputRow } from './scripts/game-board/get-word-from-input-row.js';
 import { enableNextRow } from './scripts/game-board/enable-next-row.js';
-import { blockButton } from './scripts/game-board/block-button.js';
-
 
 createBoard();
 let currentRowIndex = 0;
@@ -17,12 +15,10 @@ const gameBoardInputs = document.querySelectorAll('.game-board__input');
 const inputRows = document.querySelectorAll('.game-board__row');
 const checkButton = document.querySelector('.keyboard__button-check');
 // при инициализации кнопка недоступна
-blockButton(checkButton, 'keyboard__button-check-disabled')
+checkButton.classList.add('keyboard__button-check-disabled')
 
-// Блокируем все ряды, кроме первого
-blockInputRow(inputRows, 1);
+gameResultShow(false);
 
-// Привязываем обработчики событий к инпутам каждого ряда
 inputRows.forEach((row, rowIndex) => {
   const rowInputs = row.querySelectorAll('.game-board__input');
 
@@ -36,69 +32,34 @@ inputRows.forEach((row, rowIndex) => {
 
   // Если это не первый ряд, то изначально блокируем инпуты
   if (rowIndex > 0) {
-    blockInputRow(inputRows, rowIndex);
+    blockInputRows(inputRows, rowIndex);
   }
 });
 
-const colorizeLetters = (word, correctWord) => {
-  const correct = [];
-  const misplaced = [];
-  const absent = [];
-
-  const correctWordArr = correctWord.split('');
-  const wordArr = word.split('');
-
-  wordArr.forEach((letter, index) => {
-    if (letter === correctWordArr[index]) {
-      correct.push(letter);
-      correctWordArr[index] = null;
-    } else if (correctWordArr.includes(letter)) {
-      misplaced.push(letter);
-      correctWordArr[correctWordArr.indexOf(letter)] = null;
-    } else {
-      absent.push(letter);
-    }
-  });
-
-  return { correct, misplaced, absent };
-};
-
-const updateUIWithColors = (rowIndex, correct, misplaced, absent) => {
-  const inputs = inputRows[rowIndex].querySelectorAll('.game-board__input');
-  Array.from(inputs).forEach((input, index) => {
-    if (correct.includes(input.value)) {
-      input.classList.add('correct'); // Зелёный
-    } else if (misplaced.includes(input.value)) {
-      input.classList.add('misplaced'); // Жёлтый
-    } else {
-      input.classList.add('absent'); // Серый
-    }
-  });
-};
-
 checkButton.addEventListener('click', () => {
-  blockButton(checkButton, 'keyboard__button-check-disabled');
+  checkButton.classList.add('keyboard__button-check-disabled')
 
-  const word = getWordFromInputRow(inputRows[currentRowIndex - 1]);
+  const word = getWordFromInputRow(inputRows[currentRowIndex]);
   const correctWord = "фирма";
 
-  const { correct, misplaced, absent } = colorizeLetters(word, correctWord);
-
-  updateUIWithColors(currentRowIndex - 1, correct, misplaced, absent);
-
   if (word === correctWord) {
-    gameResultShow(true);
+    gameResultShow(true, true);
     return;
   }
 
-  // blockInputRow(inputRows, currentRowIndex);
-  enableNextRow(inputRows, currentRowIndex);
-  currentRowIndex++;
+  console.log({
+    currentRowIndex,
+    inputRowsLength: inputRows.length - 1
+  })
 
-  console.log(currentRowIndex, inputRows.length);
-  if (currentRowIndex === inputRows.length) {
-    gameResultShow(false);
+  if (currentRowIndex === inputRows.length - 1) {
+    gameResultShow(true, false); // Показываем экран проигрыша
+    console.log('проебал');
+    return;
   }
+
+  currentRowIndex++;
+  enableNextRow(inputRows, currentRowIndex);
 });
 
 validateInput(gameBoardInputs);
--- src/main/resources/scripts/game-board/game-result.js
+++ src/main/resources/scripts/game-board/game-result.js
@@ -1,9 +1,19 @@
 //Функция отображения результата уровня
-export const gameResultShow = (isVisible) => {
+export const gameResultShow = (isVisible, isWin) => {
   const gameBoard = document.querySelector('.game-board');
   const keyboard = document.querySelector('.keyboard');
   const asideToolbar = document.querySelector('.aside__toolbar');
   const gameResult = document.querySelector('.game-result');
+  const gameResultText = gameResult.querySelector('.game-result__text');
+  const gameResultButton = gameResult.querySelector('.game-result__button');
+
+  if (!isWin) {
+    gameResultText.textContent = 'Следующий уровень?';
+    gameResultButton.textContent = 'Попробовать снова?';
+  } else {
+    gameResultText.textContent = 'Победа'
+    gameResultButton.textContent = 'Попробовать снова?'
+  }
 
   gameBoard.classList.toggle('hidden', isVisible);
   keyboard.classList.toggle('hidden', isVisible);

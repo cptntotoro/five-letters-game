00de24c93e5d15d95ae2557833693f02991e2371
GROUL46 <82001055+GROUL46@users.noreply.github.com>
2024-12-18 03:39:14 +0300

    Изменен возврат значений
    Добавлен объект

--- src/main/resources/getting_letters.js
+++ src/main/resources/getting_letters.js
@@ -3,20 +3,10 @@ function guessLetters(word1, word2) {
   const targetWord = word1.toLowerCase();
   const guessWord = word2.toLowerCase();
 
-  // Объявляем объект массивов
-  let obj_guess_mass = {
-    arrayOne: []
-  }
-
   // Объявляем массивы
   let correctPosition = [];
   let incorrectPosition = [];
   let notInTarget = [];
-
-  // Добавляем массивы в объект
-  obj_guess_mass.arrayOne.push(correctPosition);
-  obj_guess_mass.arrayOne.push(incorrectPosition);
-  obj_guess_mass.arrayOne.push(notInTarget);
 
   // Проверяем буквы в слове
   for (let i = 0; i < guessWord.length; i++) {
@@ -32,15 +22,12 @@ function guessLetters(word1, word2) {
     }
   }
 
-  return obj_guess_mass.arrayOne;
-}
+  // Объявляем объект
+  const result_guess = {
+    correctPosition,
+    incorrectPosition,
+    notInTarget
+  };
 
-try {
-  const result = guessLetters(word1, word2);
-  console.log('Буквы на своих позициях:', result[0].join(','));
-  console.log('Буквы в слове, но не на своих позициях:', result[1].join(','));
-  console.log('Буквы не в слове:', result[2].join(','));
-}
-catch (error) {
-  console.error(error.message);
+  return result_guess;
 }

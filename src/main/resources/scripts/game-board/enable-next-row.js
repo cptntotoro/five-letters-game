a286cd601360d6ebbc349e7ffe1776108367dc31
Eugene <hidden@mail.ru>
2024-12-18 21:50:03 +0300

    feature/20: расмширен и модифицирован функционал показа экрана результата игры

--- src/main/resources/scripts/game-board/enable-next-row.js
+++ src/main/resources/scripts/game-board/enable-next-row.js
@@ -1,12 +1,5 @@
 export const enableNextRow = (inputRows, currentRowIndex) => {
-  console.log('сработала функция открытия следующего ряда');
-
-  console.log({
-    inputRows,
-    currentRowIndex
-  });
-
-  if (currentRowIndex + 1 <= inputRows.length) {
+  if (currentRowIndex <= inputRows.length) {
     const nextRowInputs = inputRows[currentRowIndex].querySelectorAll('.game-board__input');
     nextRowInputs.forEach(input => {
       input.removeAttribute('disabled');

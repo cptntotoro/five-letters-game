b9fc999b72d29d455030833d6b7e5658ad431dc9
Eugene <hidden@mail.ru>
2024-12-21 18:32:12 +0300

    feature/20: рефакторинг, добавлены функции раскраски, добавлено инициализация приложения, удалены предыдущие файлы

--- src/main/resources/scripts/game-board/block-input-row.js
+++
@@ -1,6 +0,0 @@
-export const blockInputRows = (inputRows, currentRowIndex) => {
-  const currentRowInputs = inputRows[currentRowIndex].querySelectorAll('.game-board__input');
-  currentRowInputs.forEach(input => {
-    input.setAttribute('disabled', true);
-  });
-};

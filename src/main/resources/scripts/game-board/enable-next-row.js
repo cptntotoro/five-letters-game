315a4509e3cf9d61a800bc996c47071dc84c2d77
Eugene <hidden@mail.ru>
2024-12-21 18:32:12 +0300

    feature/20: рефакторинг, добавлены функции раскраски, добавлено инициализация приложения, удалены предыдущие файлы

--- src/main/resources/scripts/game-board/enable-next-row.js
+++
@@ -1,8 +0,0 @@
-export const enableNextRow = (inputRows, currentRowIndex) => {
-  if (currentRowIndex <= inputRows.length) {
-    const nextRowInputs = inputRows[currentRowIndex].querySelectorAll('.game-board__input');
-    nextRowInputs.forEach(input => {
-      input.removeAttribute('disabled');
-    });
-  }
-};
--- src/main/resources/scripts/game-board/loader.js
+++ src/main/resources/scripts/game-board/loader.js
@@ -1,12 +1,13 @@
-// Функция отображения лоадера
 export function renderLoading(isLoading) {
   const gameBoard = document.querySelector('.game-board');
   const toolbar = document.querySelector('.aside__toolbar');
   const loader = document.querySelector('.loader__container');
   const gameResult = document.querySelector('.game-result');
+  const keyboard = document.querySelector('.keyboard');
 
   loader.classList.toggle('invisible', !isLoading);
   gameBoard.classList.toggle('invisible', isLoading);
   toolbar.classList.toggle('invisible', isLoading);
   gameResult.classList.toggle('invisible', isLoading);
+  keyboard.classList.toggle('invisible', isLoading);
 }
